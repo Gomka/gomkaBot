3887c50ff5a84e999c57c81794a0938fcb560eda
Gonzalo <gmarcosvaquero@gmail.com>
2019-11-27 10:38:38 +0100

    Removed on ready y mejorados los dubs

--- bot.js
+++ bot.js
@@ -1,15 +1,6 @@
 const Discord = require('discord.js');
 
 const bot = new Discord.Client();
- 
-
-bot.on('ready', () => {
-
-    console.log('I am ready!');
-
-});
-
- 
 
 bot.on('message', message => {
 
@@ -17,13 +8,13 @@ bot.on('message', message => {
 
     if (message.content === 'ping') {
 
-       message.channel.send("pong")
+        message.channel.send("pong")
 
     }
 
     if (message.content === 'test') {
-        
-        message.channel.send('A <@' + message.author.id +'> le pica la cabeza por dentro.');
+
+        message.channel.send('A <@' + message.author.id + '> le pica la cabeza por dentro.');
 
     }
 
@@ -33,17 +24,45 @@ bot.on('message', message => {
 
     }
 
-    if (input.includes("dubs")) {
+    if (input === "dubs") {
 
-        var dubs = Math.floor(Math.random() * 1000000000);
+        var dubs = Math.floor(Math.random() * 1000000000).toString();
 
-        message.channel.send(dubs);
-        
+        var tot = 0;
+
+        var test = "";
+
+        for (var i = dubs.length-1; i > 0; i--) {
+            
+            if(dubs.charAt(i) == dubs.charAt(i+1)) tot++;
+            test += dubs.charAt(i+1);
+            
+        }
+
+        switch (tot) {
+            case 1: message.reply(dubs + " check them dubs");                
+                break;
+            case 2: message.reply(dubs + " bruh trips checked");                
+                break;
+            case 3: message.reply(dubs + " el diablo that be some quads");                
+                break;
+            case 4: message.reply(dubs + " the fuck man you hackin' that be quints");                
+                break
+            case 5: message.reply(dubs + " six in a row, you'r'e mams a hoe");                
+                break;
+            case 6: message.reply(dubs + " seven eleven this string is probably never getting printed");                
+                break;
+            case 7: message.reply(dubs + " eight in a row. Congratulations you hacked my bot or something");                
+                break;
+            case 8: message.reply(dubs + " go buy some lotto");                
+                break;
+            default: message.channel.send(dubs + " " + test);
+                break;
+        }
+
     }
 
-}); 
-
-
+});
 
 // THIS  MUST  BE  THIS  WAY
 
