ccf3c693c3033b37b832287232e0339dd690ef82
Gonzalo <gmarcosvaquero@gmail.com>
2020-01-08 10:07:59 +0100

    test 2

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ bot.on('ready', () => {
     console.log('c biene');
 
     // bot.user.setActivity('${bot.guilds.size} children cum', { type: 'WATCHING' });
-    bot.user.setPresence({ game: { name: 'test', type: 0}});
+    bot.user.setPresence({ game: { name: 'test', type: 1}});
 });
 
 bot.on("guildCreate", guild => {
