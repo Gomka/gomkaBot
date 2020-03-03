af07a5ce5ab824ae9566c6dadc8d210b0f922dca
Gonzalo <gmarcosvaquero@gmail.com>
2020-03-03 11:45:21 +0100

    Fixin' makes me feel good

--- bot.js
+++ bot.js
@@ -140,46 +140,6 @@ bot.on('message', async message => {
         }
     }
 
-    if (messageStringsLower[0] === "roll") {
-
-        var dubs = message.id;
-        var tot = 0;
-
-        for (var i = dubs.length - 1; i > 0; i--) {
-
-            if (dubs.charAt(i - 1) == dubs.charAt(i)) {
-                tot++;
-            }
-            else {
-                break;
-            }
-
-        }
-
-        switch (tot) {
-            case 1: message.reply(dubs + " check them dubs");
-                break;
-            case 2: message.reply(dubs + " bruh trips checked");
-                break;
-            case 3: message.reply(dubs + " el diablo that be some quads");
-                break;
-            case 4: message.reply(dubs + " the fuck man you hackin' that be quints");
-                break
-            case 5: message.reply(dubs + " six in a row, you'r'e mams a hoe");
-                break;
-            case 6: message.reply(dubs + " seven eleven this string is probably never getting printed");
-                break;
-            case 7: message.reply(dubs + " eight in a row. Loko ke marrrdito ratatwuá");
-                break;
-            case 8: case 9: case 10: case 11: case 12: case 13: case 14: case 15: case 16: case 17:
-            case 18: message.reply(dubs + "... go buy some lotto");
-                break;
-            default: message.channel.send(dubs);
-                break;
-        }
-
-    }
-
     function robaladaAdd(shiny) {
 
         if (shiny) {
@@ -212,7 +172,7 @@ bot.on('message', async message => {
 
                     var length = robaladaShinyList.length - 1;
 
-                    message.channel.send("`Robalada nº" + length + " satisfactoriamente sintetizada.`");
+                    message.channel.send("`Robalada shiny nº" + length + " satisfactoriamente sintetizada.`");
 
                 }
                 catch (error) {
@@ -292,7 +252,7 @@ bot.on('message', async message => {
 
                         robaladaShinyList.splice(posicionABorrar, 1);
 
-                        message.channel.send("Robalada cleansed successfully");
+                        message.channel.send("`Robalada cleansed successfully`");
 
                     } else {
 
@@ -325,7 +285,7 @@ bot.on('message', async message => {
 
                         robaladaList.splice(posicionABorrar, 1);
 
-                        message.channel.send("Robalada cleansed successfully");
+                        message.channel.send("`Robalada cleansed successfully`");
 
                     } else {
 
@@ -471,13 +431,53 @@ bot.on('message', async message => {
 
                 index = Math.floor(Math.random() * robaladaList.length);
 
-                message.channel.send("`" + index + ":`" + robaladaList[index]);
+                message.channel.send("`" + index + ":` " + robaladaList[index]);
 
             } else {
 
                 message.channel.send("No robaladas to deliver (yet)");
             }
         }
+    }
+    
+    if (messageStringsLower[0] === "roll") {
+
+        var dubs = message.id;
+        var tot = 0;
+
+        for (var i = dubs.length - 1; i > 0; i--) {
+
+            if (dubs.charAt(i - 1) == dubs.charAt(i)) {
+                tot++;
+            }
+            else {
+                break;
+            }
+
+        }
+
+        switch (tot) {
+            case 1: message.reply(dubs + " check them dubs");
+                break;
+            case 2: message.reply(dubs + " bruh trips checked");
+                break;
+            case 3: message.reply(dubs + " el diablo that be some quads");
+                break;
+            case 4: message.reply(dubs + " the fuck man you hackin' that be quints");
+                break
+            case 5: message.reply(dubs + " six in a row, you'r'e mams a hoe");
+                break;
+            case 6: message.reply(dubs + " seven eleven this string is probably never getting printed");
+                break;
+            case 7: message.reply(dubs + " eight in a row. Loko ke marrrdito ratatwuá");
+                break;
+            case 8: case 9: case 10: case 11: case 12: case 13: case 14: case 15: case 16: case 17:
+            case 18: message.reply(dubs + "... go buy some lotto");
+                break;
+            default: message.channel.send(dubs);
+                break;
+        }
+
     }
 
 });
