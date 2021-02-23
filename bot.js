e2470d427d7256d4304077fdc06240a66e27aede
Gonzalo Marcos Vaquero <55784961+Gomka@users.noreply.github.com>
2021-02-23 11:54:31 +0100

    async/await

--- bot.js
+++ bot.js
@@ -149,7 +149,7 @@ bot.on('message', async message => {
 
                     robaladaStr = robaladaStr.replace("'", "");
 
-                    addRobalada(robaladaStr, shiny === true ? 1 : 0); //1 for shiny, 0 for regular
+                    await addRobalada(robaladaStr, shiny === true ? 1 : 0); //1 for shiny, 0 for regular
 
                     robaladaShinyList.push([robaladaStr, "*"]);
 
@@ -179,7 +179,7 @@ bot.on('message', async message => {
 
                     robaladaStr = robaladaStr.replace("'", "");
 
-                    addRobalada(robaladaStr, shiny === true ? 1 : 0); // 1 for shiny, 0 for regular
+                    await addRobalada(robaladaStr, shiny === true ? 1 : 0); // 1 for shiny, 0 for regular
 
                     robaladaList.push([robaladaStr, "*"]);
 
@@ -213,7 +213,7 @@ bot.on('message', async message => {
 
                     if (!isNaN(posicionABorrar) && posicionABorrar >= 0 && posicionABorrar < robaladaShinyList.length) {
 
-                        deleteRobalada(posicionABorrar, shiny === true ? 1 : 0) // 1 for shiny, 0 for regular
+                        await deleteRobalada(posicionABorrar, shiny === true ? 1 : 0) // 1 for shiny, 0 for regular
 
                         robaladaShinyList.splice(posicionABorrar, 1);
 
@@ -243,7 +243,7 @@ bot.on('message', async message => {
 
                     if (!isNaN(posicionABorrar) && posicionABorrar >= 0 && posicionABorrar < robaladaList.length) {
 
-                        deleteRobalada(posicionABorrar, shiny === true ? 1 : 0) // 1 for shiny, 0 for regular
+                        await deleteRobalada(posicionABorrar, shiny === true ? 1 : 0) // 1 for shiny, 0 for regular
 
                         robaladaList.splice(posicionABorrar, 1);
 
